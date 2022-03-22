7eaacdc6652770f0261595d96c60bd21fb039f57
Angel Luis Ortiz Folgado <angelluisortiz1990@gmail.com>
2022-03-22 17:54:50 +0100

    Prueba parpadeo nodemcu ESP8266

---
+++ POCs/prueba_parpadeo.ino
@@ -0,0 +1,29 @@
+#include "DHT.h"
+#define DHTPIN 5
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+Serial.begin(9600);
+Serial.println(F("DHTxx test!"));
+
+dht.begin();
+}
+
+void loop() {
+delay(2000); //Es un sensor lento, por lo que hay que darle tiempo.
+float h = dht.readHumidity();
+float t = dht.readTemperature();
+
+if (isnan(h) || isnan(t)) {
+Serial.println(F("Failed to read from DHT sensor!"));
+return;
+}
+
+Serial.print(F("Humedad: "));
+Serial.print(h);
+Serial.print(F("% Temperatura: "));
+Serial.print(t);
+Serial.println(F("°C "));
+}
